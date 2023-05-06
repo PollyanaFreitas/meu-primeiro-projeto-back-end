edeed20f51f2eca07b9f8679ca2ed8a8d3ac29b6
PollyanaFreitas <pollyana.freitaas@gmail.com>
2023-05-06 08:14:37 -0300

    "Mudanca de link imagem"

--- mulher.js
+++ mulher.js
@@ -7,12 +7,10 @@ const porta = 3333
 function mostraMulher(request, response) {
     response.json({
         nome: 'Polyana de Freitas',
-        imagem: 'https:www.instagram.com/pollyanafreitass/',
+        imagem: 'https://media.licdn.com/dms/image/C4D03AQHvIaN5a2vzZw/profile-displayphoto-shrink_100_100/0/1639733520464?e=1688601600&v=beta&t=Dj2aoNe4XLcSJk9lV9oKJ36fDihDRd1qehyhrlwQDiE',
         minibio:'Desenvolvedora de sistemas'
-
+      
     })
-
-
 }
 
 function mostraPorta() {
@@ -21,3 +19,6 @@ function mostraPorta() {
 
 app.use(router.get('/mulher',mostraMulher))
 app.listen(porta, mostraPorta)
+
+
+
